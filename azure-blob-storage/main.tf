dcec79af54d56ed4093f27487721fc84b41ba716
Alhen1910 <127019203+Alhen1910@users.noreply.github.com>
2024-05-05 19:13:11 +0200

    Update main.tf
    corrected name referrences

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "resourcegroup2" {
 
 resource "azurerm_storage_account" "StorageAccount2" {
   name                     = var.blob_storage_storage_account_name
-  resource_group_name      = azurerm_resource_group.resourcegroup1.name
-  location                 = azurerm_resource_group.resourcegroup1.location
+  resource_group_name      = azurerm_resource_group.resourcegroup2.name
+  location                 = azurerm_resource_group.resourcegroup2.location
   account_tier             = var.blob_storage_storage_account_tier
   account_replication_type = var.blob_storage_storage_account_replication_type
 
@@ -17,14 +17,14 @@ resource "azurerm_storage_account" "StorageAccount2" {
 
 resource "azurerm_storage_container" "StorageContainer1" {
   name                  = var.storage_container_name
-  storage_account_name  = azurerm_storage_account.storagecontainer2.name
+  storage_account_name  = azurerm_storage_account.StorageAccount2.name
   container_access_type = var.storage_container_access_type
 }
 
 resource "azurerm_storage_blob" "StorageBlob1" {
   name                   = var.storage_blob_name
-  storage_account_name   = azurerm_storage_account.storagecontainer2.name
-  storage_container_name = azurerm_storage_container.storagecontainer1.name
+  storage_account_name   = azurerm_storage_account.StorageAccount2.name
+  storage_container_name = azurerm_storage_container.StorageContainer1.name
   type                   = var.storage_blob_type
   source                 = var.storage_blob_source
 }
