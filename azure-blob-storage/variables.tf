85f776819a073a459c105fec913d0c000029445c
Alhen1910 <127019203+Alhen1910@users.noreply.github.com>
2024-05-05 18:53:59 +0200

    Update variables.tf
    added "blob_storage_" prefix to variable names

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -1,29 +1,29 @@
-variable "resource_group_name" {
+variable "blob_storage_resource_group_name" {
 	type = string
 	default = "resourcegroup2"
 }
 
-variable "resource_group_location" {
+variable "blob_storage_resource_group_location" {
 	type = string
 	default = "West Europe"
 }
 
-variable "storage_account_name" {
+variable "blob_storage_storage_account_name" {
 	type = string
 	default = "storageaccount2"
 }
 
-variable "storage_account_tier" {
+variable "blob_storage_storage_account_tier" {
 	type = string
 	default = "Standard"
 }
 
-variable "storage_account_replication_type" {
+variable "blob_storage_storage_account_replication_type" {
 	type = string
 	default = "GRS"
 }
 
-variable "storage_account_environment" {
+variable "blob_storage_storage_account_environment" {
 	type = string
 	default = "staging"
 }
