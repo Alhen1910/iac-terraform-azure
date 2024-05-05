32b250c0478e514fbe13867556349d87c032fea1
Alhen1910 <127019203+Alhen1910@users.noreply.github.com>
2024-05-05 18:11:44 +0200

    Update main.tf
    added azurerm_service_plan

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -14,3 +14,11 @@ resource "azurerm_storage_account" "StorageAccount1" {
     environment = "staging"
   }
 }
+
+resource "azurerm_service_plan" "ServicePlan1" {
+  name                = "serviceplan1"
+  resource_group_name = azurerm_resource_group.resourcegroup1.name
+  location            = azurerm_resource_group.resourcegroup1.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
