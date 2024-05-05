904fdc8c206762a3936919a4e696ef72f9ad1d96
Alhen1910 <127019203+Alhen1910@users.noreply.github.com>
2024-05-05 18:55:31 +0200

    Update main.tf
    configured  azurerm_resource_group and azurerm_storage_account

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -1,1 +1,16 @@
+resource "azurerm_resource_group" "resourcegroup2" {
+  name     = var.blob_storage_resource_group_name
+  location = var.blob_storage_resource_group_location
+}
 
+resource "azurerm_storage_account" "StorageAccount2" {
+  name                     = var.blob_storage_storage_account_name
+  resource_group_name      = azurerm_resource_group.resourcegroup1.name
+  location                 = azurerm_resource_group.resourcegroup1.location
+  account_tier             = var.blob_storage_storage_account_tier
+  account_replication_type = var.blob_storage_storage_account_replication_type
+
+  tags = {
+    environment = var.blob_storage_storage_account_environment
+  }
+}
