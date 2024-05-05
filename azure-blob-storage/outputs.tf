c8af88d405697a52c943f255d89e9e038f66dc79
Alhen1910 <127019203+Alhen1910@users.noreply.github.com>
2024-05-05 19:10:29 +0200

    Update outputs.tf
    corrected a typo

--- azure-blob-storage/outputs.tf
+++ azure-blob-storage/outputs.tf
@@ -1,4 +1,4 @@
-output "azurem_storage_blob_url" {
+output "azurerm_storage_blob_url" {
   description = "Display url for azurem_storage_blob"
   value       = azurem_storage_blob.StorageBlob1.endpoint
 }
